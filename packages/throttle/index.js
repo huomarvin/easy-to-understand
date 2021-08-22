9891f1a536dc12b16bd3fe1d5cefb5554061dae3
xhh <huo.marvin@gmail.com>
2021-08-23 00:28:30 +0800

    fix: 处理节流情况下单次触发不执行问题

--- packages/throttle/index.js
+++ packages/throttle/index.js
@@ -1,11 +1,19 @@
-function throttle(func, period = 2000) {
+/**
+ * @param {Function} func
+ * @param {number} period
+ * @returns Function
+ */
+function throttle(func, period = 500) {
   let lastTime = new Date();
+  let timer = null;
   return function (...args) {
     const now = new Date();
-    if (now - lastTime > period) {
-      func.call(this, ...args);
-      lastTime = new Date();
-      return;
+    if (!timer && now - lastTime > period) {
+      timer = setTimeout(() => {
+        func.call(this, ...args);
+        lastTime = new Date();
+        timer = null;
+      }, period);
     }
   };
 }
